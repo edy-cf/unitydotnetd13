35ec79e0bd235cebcfc6122e8c2cd0e7fd5e5c16
Ed Younskevicius <ed@codefellows.com>
2021-09-07 10:52:48 -0700

    Initial Prototype 3

---
+++ New Unity Project/Assets/Scripts/BrickCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickCube : MonoBehaviour
+{
+  public Vector3 rotationChange;
+  
+  // Start is called before the first frame update
+  void Start()
+  {
+    
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    transform.Rotate(rotationChange);
+    Debug.Log("Rotation is: " + rotationChange);
+  }
+}
---
+++ New Unity Project/Assets/Scripts/PlayerController3.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController3 : MonoBehaviour
+{
+  public float jumpForce;
+  public float gravityModifier;
+  public bool isOnGround = true;
+  
+  private Rigidbody _playerRb;
+  
+  // Start is called before the first frame update
+  void Start()
+  {
+    _playerRb = GetComponent<Rigidbody>();
+    Physics.gravity *= gravityModifier;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+    {
+      _playerRb.AddForce(Vector3.up * jumpForce);
+      isOnGround = false;
+    }
+  }
+
+  private void OnCollisionEnter(Collision collision)
+  {
+    isOnGround = true;
+  }
+}
